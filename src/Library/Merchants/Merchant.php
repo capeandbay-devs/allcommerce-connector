d3c1483f2099650d8d74e8753eadf45f99cc1e8b
Angel Gonzalez <angel@capeandbay.com>
2020-06-13 01:15:04 -0400

    New Merchnt Support
    Heyo!

--- src/Library/Merchants/Merchant.php
+++ src/Library/Merchants/Merchant.php
@@ -7,9 +7,62 @@ use CapeAndBay\AllCommerce\Library\Feature;
 class Merchant extends Feature
 {
     protected $url = '/merchants';
+    protected $access_token;
+    protected $details;
 
     public function __construct($presets = [])
     {
         parent::__construct();
     }
+
+    public function merchant_url()
+    {
+        return $this->allcommerce_client->api_url().$this->url;
+    }
+
+    public function shopify_merchant_url()
+    {
+        return $this->allcommerce_client->api_url().'/shopify'.$this->url;
+    }
+
+    public function init()
+    {
+        if($results = $this->getMerchant())
+        {
+            $this->details = $results;
+        }
+        else
+        {
+            $this->details = null;
+        }
+    }
+
+    public function getMerchant()
+    {
+        $results = false;
+
+        try
+        {
+            $headers = [
+                'Accept: vnd.allcommerce.v1+json',
+                'Authorization: Bearer '.session()->get('allcommerce-jwt-access-token')
+            ];
+            $response = $this->allcommerce_client->get($this->merchant_url(), [], $headers);
+
+            if($response)
+            {
+                if(array_key_exists('success', $response) && $response['success'])
+                {
+                    $results = $response['merchant'];
+                }
+            }
+        }
+        catch(\Exception $e)
+        {
+            $results = $e->getMessage();
+        }
+
+        return $results;
+    }
+
 }
--- src/config/allcommerce-jwt.php
+++ src/config/allcommerce-jwt.php
@@ -3,6 +3,7 @@
 return [
     'class_maps' => [
         'account-profile' => \CapeAndBay\AllCommerce\Library\Account\ProfileInformation::class,
-        'inventory' => \CapeAndBay\AllCommerce\Library\Inventory\MerchantInventory::class
+        'inventory' => \CapeAndBay\AllCommerce\Library\Inventory\MerchantInventory::class,
+        'merchant' => \CapeAndBay\AllCommerce\Library\Merchants\Merchant::class
     ],
 ];
